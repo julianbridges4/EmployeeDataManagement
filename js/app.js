513b934849b6db03e11c03a19fb0d156337c27c3
Devon <wooddv35@gmail.com>
2017-08-02 21:13:42 -0400

    fixed some junk

--- js/app.js
+++ js/app.js
@@ -1,41 +1,43 @@
-$(document).on("ready", function() {
-	// Initialize Firebase
-	var config = {
-	apiKey: "AIzaSyADmvtoeIRMfZ0DYvMrk8Fz_MQ2-4gX_aI",
-	authDomain: "employeedatamanagement-be900.firebaseapp.com",
-	databaseURL: "https://employeedatamanagement-be900.firebaseio.com",
-	projectId: "employeedatamanagement-be900",
-	storageBucket: "employeedatamanagement-be900.appspot.com",
-	messagingSenderId: "256758979445"
-	};
 
-	firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyADmvtoeIRMfZ0DYvMrk8Fz_MQ2-4gX_aI",
+authDomain: "employeedatamanagement-be900.firebaseapp.com",
+databaseURL: "https://employeedatamanagement-be900.firebaseio.com",
+projectId: "employeedatamanagement-be900",
+storageBucket: "employeedatamanagement-be900.appspot.com",
+messagingSenderId: "256758979445"
+};
 
-	var database = firebase.database();
+firebase.initializeApp(config);
 
-	var userName = '';
-	var userRole = '';
-	var userStart = 0;
-	var userRate = 0;
+var database = firebase.database();
 
-	$('.btn').on('click', function() {
-		event.preventDefault();
+var userName = '';
+var userRole = '';
+var userStart = 0;
+var userRate = 0;
 
-		// Capture user input values
-		userName = $('#employee-name').val().trim();
-		userRole = $('#role').val().trim();
-		userStart = $('#start-date').val().trim();
-		userRate = $('#month-rate').val().trim();
+$('.btn').on('click', function() {
+	event.preventDefault();
 
-		// Store inputs into firebase database
-		database.ref().push({
-			nameFB: userName,
-			roleFB: userRole,
-			startFB: userStart,
-			rateFB: userRate,
-			dateAdded: firebase.database.ServerValue.TIMESTAMP
-		})
+	// Capture user input values
+	userName = $('#employee-name').val().trim();
+	userRole = $('#role').val().trim();
+	userStart = $('#start-date').val().trim();
+	userRate = $('#month-rate').val().trim();
+	console.log(userName);
+	// Store inputs into firebase database
+	database.ref().push({
+		nameFB: userName,
+		roleFB: userRole,
+		startFB: userStart,
+		rateFB: userRate,
+		dateAdded: firebase.database.ServerValue.TIMESTAMP
+	})
 
-	});
+	$('#employee-name, #role, #start-date, #month-rate').val('');
 
 });
+
+

a89855427523bbd869ee40403482ea0e3ac8c96c
Kristin Glaze <ikglaze33@gmail.com>
2017-08-02 20:08:23 -0400

    Added more external linking. app.js is linked up with index.html, and included firebase first code that I had to copy paste.

---
+++ js/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyADmvtoeIRMfZ0DYvMrk8Fz_MQ2-4gX_aI",
+    authDomain: "employeedatamanagement-be900.firebaseapp.com",
+    databaseURL: "https://employeedatamanagement-be900.firebaseio.com",
+    projectId: "employeedatamanagement-be900",
+    storageBucket: "employeedatamanagement-be900.appspot.com",
+    messagingSenderId: "256758979445"
+  };
+  firebase.initializeApp(config);
